343c18b7d6b810f3b77e9d9a45e8207491392259
Robert Luke <robert.g.luke@gmail.com>
2014-11-07 20:26:22 +0100

    Split spectrum code out to make faster

--- src/statistics/hotelling.jl
+++ src/statistics/hotelling.jl
@@ -11,7 +11,7 @@ using Docile
 Hotelling test on SSR data
 Saves results in a.processing["hotelling#"]
 """ ->
-function hotelling(a::SSR, freq_of_interest::Number; ID::String="", kwargs...)
+function hotelling(a::SSR; freq_of_interest::Union(Real, AbstractArray)=float(a.modulation_frequency), ID::String="", kwargs...)
 
     # TODO: Account for multiple applied filters
     if haskey(a.processing, "filter1")
@@ -20,56 +20,60 @@ function hotelling(a::SSR, freq_of_interest::Number; ID::String="", kwargs...)
         used_filter = nothing
     end
 
-    snrDb, phase, signal, noise, statistic =
-        hotelling(a.processing["epochs"], freq_of_interest, int(a.sample_rate), used_filter=used_filter; kwargs...)
+    spectrum = _hotelling_spectrum(a.processing["epochs"])
 
-    result = DataFrame(
-                        ID                  = vec(repmat([ID], length(a.channel_names), 1)),
-                        Channel             = copy(a.channel_names),
-                        ModulationFrequency = copy(float(a.modulation_frequency)),
-                        AnalysisType        = "hotelling",
-                        AnalysisFrequency   = freq_of_interest,
-                        SignalPower         = vec(signal),
-                        SignalPhase         = vec(phase),
-                        NoisePower          = convert(Array{FloatingPoint}, noise),
-                        SNRdB               = convert(Array{FloatingPoint}, snrDb),
-                        Statistic           = vec(statistic)
-                      )
+    for freq in freq_of_interest
 
-    result = add_dataframe_static_rows(result, kwargs)
+        snrDb, phase, signal, noise, statistic =
+            hotelling(spectrum, freq, int(a.sample_rate), used_filter; kwargs...)
 
-    key_name = new_processing_key(a.processing, "hotelling")
-    merge!(a.processing, [key_name => result])
+        result = DataFrame(
+                            ID                  = vec(repmat([ID], length(a.channel_names), 1)),
+                            Channel             = copy(a.channel_names),
+                            ModulationFrequency = copy(float(a.modulation_frequency)),
+                            AnalysisType        = "hotelling",
+                            AnalysisFrequency   = freq,
+                            SignalPower         = vec(signal),
+                            SignalPhase         = vec(phase),
+                            NoisePower          = convert(Array{FloatingPoint}, noise),
+                            SNRdB               = convert(Array{FloatingPoint}, snrDb),
+                            Statistic           = vec(statistic)
+                          )
+
+        result = add_dataframe_static_rows(result, kwargs)
+
+        key_name = new_processing_key(a.processing, "hotelling")
+        merge!(a.processing, [key_name => result])
+
+    end
 
     return a
 end
 
+# Backward compatibility
+function hotelling(a::SSR, freq_of_interest::Union(Real, AbstractArray); kwargs...)
 
-# If more than one frequency of interest is specified then run for all
-function hotelling(a::SSR, freq_of_interest::Array; kwargs...)
-
-    for f = freq_of_interest; a = hotelling(a, f; kwargs...); end; return a
-end
-
-
-# If no frequency of interest is specified then use the modulation frequency
-function hotelling(a::SSR; kwargs...)
-
-    hotelling(a, float(a.modulation_frequency); kwargs...)
+    hotelling(a; freq_of_interest=freq_of_interest, kwargs...)
 end
 
 
 
 
-function hotelling(epochs::Array, freq_of_interest::Number, fs::Number;
-                   used_filter=nothing, kwargs...)
+function hotelling(epochs::Union(Array{Float64, 3}, Array{Float32, 3}), args...; kwargs...)
+
+    hotelling(_hotelling_spectrum(epochs), args...; kwargs...)
+end
 
 
-    spectrum    = _hotelling_spectrum(epochs)
-    frequencies = linspace(0, 1, int(size(epochs,1) / 2 + 1))*fs/2
+
+
+function hotelling(spectrum::Union(Array{Complex{Float64},3}, Array{Complex{Float32},3}), freq_of_interest::Real, fs::Real, used_filter::Union(Filter, Nothing); kwargs...)
+
+    frequencies = linspace(0, 1, int(size(spectrum, 1)))*fs/2
     idx         = _find_closest_number_idx(frequencies, freq_of_interest)
 
-    info("Calculating hotelling statistic on $(size(epochs)[end]) channels at $freq_of_interest Hz with $(size(epochs)[2]) epochs")
+
+    info("Calculating hotelling statistic on $(size(spectrum)[end]) channels at $freq_of_interest Hz with $(size(spectrum)[2]) epochs")
 
     # Compensate for filter response
     if !(used_filter == nothing)
@@ -90,7 +94,7 @@ function hotelling(epochs::Array, freq_of_interest::Number, fs::Number;
     for i in 1:size(bins,3)
         d = [squeeze(bins[1, :, i], [1, 3]), mean(squeeze(bins[1, :, i], [1, 3]))]
         d = std(d)                      # epoch_noise_amplitude
-        d = d / sqrt(size(epochs,2))    # recording_noise_amplitude
+        d = d / sqrt(size(spectrum, 2)) # recording_noise_amplitude
         d = real(d .^2)                 # recording_noise_power
         push!(noise_power, d)
     end
@@ -128,7 +132,7 @@ end
 
 
 
-# Calculates the spectrum for ftest and plotting
+# Calculates the spectrum for hotelling
 function _hotelling_spectrum(sweep::Array{Float64,3}; ref::Int=0)
     # First dimension is samples, second dimension if existing is channels
 
@@ -154,5 +158,4 @@ end
 function second_moment(v)
 
     sqrt(sum((v - mean(v)).^2) / (length(v)))
-
 end
